ea1f5a6d5520d27238120ec493e01e2922136482
José Quintinno <josequintino@hotmail.com.br>
2025-06-03 13:37:22 -0300

    refactor: Implementar ajustes no Tabmenu

--- src/app/component/tabmenu/tabmenu.page.ts
+++ src/app/component/tabmenu/tabmenu.page.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonIcon, IonLabel, IonTab, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonIcon, IonFab, IonFabButton, IonLabel, IonTab, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tabmenu',
   templateUrl: './tabmenu.page.html',
   styleUrls: ['./tabmenu.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonTab, IonTabBar, IonTabs, IonTabButton, IonIcon, IonLabel]
+  imports: [IonContent, IonFab, IonFabButton, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonTab, IonTabBar, IonTabs, IonTabButton, IonIcon, IonLabel]
 })
 export class TabmenuPage implements OnInit {
 
