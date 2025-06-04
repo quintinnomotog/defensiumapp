9c8ccf82e3b4d90f8f549aada0eeca84d939bde5
José Quintinno <josequintino@hotmail.com.br>
2025-06-04 19:14:56 -0300

    feature: Copiar dados para área de transferência

--- src/app/page/vault/vault.page.ts
+++ src/app/page/vault/vault.page.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 import { IonBackButton, IonButton, IonButtons, IonCol, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonRow, IonSearchbar, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import * as icons from 'ionicons/icons';
@@ -21,29 +22,48 @@ export class VaultPage implements OnInit {
       color: '#EA4C89',
       title: 'Dribbble',
       email: 'josequintino@hotmail.com.br',
-      lastUsed: '11 Minutes'
+      lastUsed: '11 Minutes',
+      password: '#EA4C89'
     },
     {
       icon: 'logo-facebook',
       color: '#1877F2',
       title: 'Facebook',
       email: 'quintino.fb@example.com',
-      lastUsed: '2 Hours'
+      lastUsed: '2 Hours',
+      password: '#1877F2'
     },
     {
       icon: 'logo-instagram',
       color: '#E1306C',
       title: 'Instagram',
       email: 'quintino.insta@example.com',
-      lastUsed: 'Yesterday'
+      lastUsed: 'Yesterday',
+      password: '#E1306C'
     }
   ];
+
+  private toastController = inject(ToastController);
 
   constructor() {
     addIcons(icons);
   }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  public async copiarSenhaClipboard(senha: any) {
+    await navigator.clipboard.writeText(senha);
+    this.showToast(senha);
+  }
+
+  private async showToast(senha: any) {
+    const toast = await this.toastController.create({
+      message: `Senha (${senha}) copiada com Sucesso!`,
+      duration: 3000,
+      position: 'top',
+      color: 'secondary'
+    });
+    await toast.present();
   }
 
 }
