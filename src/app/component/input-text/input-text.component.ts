c9506944ce703be657079aa8a94cee3304619b4b
José Quintinno <josequintino@hotmail.com.br>
2025-06-05 17:49:06 -0300

    feature: Implementar componente de entrada de dados

--- src/app/component/input-text/input-text.component.ts
+++ src/app/component/input-text/input-text.component.ts
@@ -1,28 +1,42 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonLabel } from "@ionic/angular/standalone";
+import { IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-input-text',
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.scss'],
-  imports: [IonLabel]
+  imports: [IonLabel],
 })
-export class InputTextComponent  implements OnInit {
+export class InputTextComponent implements OnInit {
+  @Input()
+  public type: string = 'text';
 
   @Input()
-  public type: string = "text";
+  public label: string = '';
 
   @Input()
-  public label: string = "";
+  public animacao: boolean = true;
 
   public isFoco: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
+
+  public onFoco() {
+    if (this.animacao) {
+      this.isFoco = true;
+    }
+  }
 
   public onHabilitarFoco(event: any) {
-    return !event.target.value ? this.isFoco = false : true;
+    if (!this.animacao) {
+      return;
+    }
+
+    if (!event.target.value) {
+      this.isFoco = false;
+    }
   }
 
 }
