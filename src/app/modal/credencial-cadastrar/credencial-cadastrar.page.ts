62dbbd0218b7265afa321fd2b3815ddff29c9e07
José Quintinno <josequintino@hotmail.com.br>
2025-06-19 19:57:25 -0300

    feature: Implementar tela de cadastro de credencial

--- src/app/modal/credencial-cadastrar/credencial-cadastrar.page.ts
+++ src/app/modal/credencial-cadastrar/credencial-cadastrar.page.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-credencial-cadastrar',
   templateUrl: './credencial-cadastrar.page.html',
   styleUrls: ['./credencial-cadastrar.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 export class CredencialCadastrarPage implements OnInit {
 
