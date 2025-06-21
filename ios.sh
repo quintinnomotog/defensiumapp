500d3d055f802aeeedeefde34b402f7def959f8e
Quintinno <quintinno.motog@gmail.com>
2025-06-21 13:12:01 -0300

    build: Gerar artefato Ios

--- ios.sh
+++ ios.sh
@@ -2,8 +2,16 @@ rm -rf www
 rm -rf ios
 rm -rf node_modules
 npm install
+brew install ruby
+sudo gem install cocoapods
+npm run build
+npx cap copy ios
 npx npm run build --  --configuration=production
 npm install @capacitor/core@7.4.0 @capacitor/ios@7.4.0
 npx cap add ios
 npx cap sync --inline
 npx cap open ios
+
+
+  git config --global user.email "quintinno.motog@gmail.com"
+  git config --global user.name "Quintinno"
