a68381593371ab09840889f4ce4a2178d396d881
José Quintinno <josequintino@hotmail.com.br>
2025-06-25 23:08:19 -0300

    feature: Implementar Categorias de Credenciais Dinâmicas

--- src/app/page/principal/principal.page.ts
+++ src/app/page/principal/principal.page.ts
@@ -1,25 +1,74 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonAvatar, IonButton, IonContent, IonHeader, IonSearchbar, IonText, IonTitle, IonToolbar, ToastController } from '@ionic/angular/standalone';
+import {
+  IonAvatar,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonSearchbar,
+  IonText,
+  IonTitle,
+  IonToolbar,
+  ToastController,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { copy, copyOutline, notifications, notificationsOutline, reload, reloadOutline } from 'ionicons/icons';
+import {
+  copy,
+  copyOutline,
+  logoGoogle,
+  logoMicrosoft,
+  logoWindows,
+  notifications,
+  notificationsOutline,
+  reload,
+  reloadOutline,
+  business,
+  card,
+  wifi,
+  server,
+  earth,
+  key,
+  lockClosed,
+  shieldCheckmark,
+  colorPalette,
+  barChart,
+  pieChart,
+  settings,
+  terminal,
+  cloud,
+} from 'ionicons/icons';
 import { CredencialService } from 'src/app/service/credencial.service';
+import { CategoriaCredencialService } from '../../service/categoria-credencial.service';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
   standalone: true,
-  imports: [HttpClientModule, IonSearchbar, IonText, IonAvatar, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule],
+  imports: [
+    HttpClientModule,
+    IonSearchbar,
+    IonText,
+    IonAvatar,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [
-    CredencialService
-  ]
+  providers: [CredencialService],
 })
 export class PrincipalPage implements OnInit {
-
   private toastController = inject(ToastController);
 
   private credencialService = inject(CredencialService);
@@ -28,12 +77,41 @@ export class PrincipalPage implements OnInit {
 
   public isAnimacaoAtivada: boolean = false;
 
+  public categoriaCredencialList: any[] = [];
+
+  private categoriaCredencialService = inject(CategoriaCredencialService);
+
   constructor() {
-    addIcons({ reload, notifications, copyOutline, notificationsOutline, reloadOutline, copy });
+    addIcons({
+      logoWindows,
+      reload,
+      notificationsOutline,
+      logoMicrosoft,
+      copyOutline,
+      logoGoogle,
+      notifications,
+      reloadOutline,
+      copy,
+      business,
+      card,
+      wifi,
+      server,
+      earth,
+      key,
+      lockClosed,
+      shieldCheckmark,
+      colorPalette,
+      barChart,
+      pieChart,
+      settings,
+      terminal,
+      cloud,
+    });
   }
 
   ngOnInit() {
     this.findAll();
+    this.recuperarCategoriaCredencial();
   }
 
   ionViewWillEnter() {}
@@ -45,15 +123,14 @@ export class PrincipalPage implements OnInit {
     this.emitirMensagemToast();
   }
 
-  public findAll(): void {  
+  public findAll(): void {
     this.credencialService.findAll().subscribe({
       next: (response: any) => {
-        console.log(response);
         this.credencialList = response.objectList;
       },
       error: (error) => {
         console.error('Erro ao buscar credenciais:', error);
-      }
+      },
     });
     this.isAnimacaoAtivada = false;
   }
@@ -64,10 +141,10 @@ export class PrincipalPage implements OnInit {
 
   public async emitirMensagemToast() {
     const toast = await this.toastController.create({
-      message: "Senha copiada para a área de transferência!",
+      message: 'Senha copiada para a área de transferência!',
       duration: 1500,
-      color: "primary",
-      position: "bottom"
+      color: 'primary',
+      position: 'bottom',
     });
     return toast.present();
   }
@@ -81,4 +158,53 @@ export class PrincipalPage implements OnInit {
     }, 2000);
   }
 
+  private recuperarCategoriaCredencial() {
+    this.categoriaCredencialService.getFindAll().subscribe({
+      next: (response) => {
+        this.categoriaCredencialList = response.map(
+          (categoriaCredencial: any) => {
+            categoriaCredencial.icone = this.getIconeAleatorio();
+            categoriaCredencial.corIcone = this.getCorPorCategoria();
+            categoriaCredencial.background = this.getCorPorCategoria();
+            return categoriaCredencial;
+          }
+        );
+        console.log(this.categoriaCredencialList);
+      },
+      error: (response) => {
+        console.error(
+          'Erro ao tentar recuperar as Categorias das Credenciais!'
+        );
+      },
+    });
+  }
+
+  private getCorPorCategoria(): string {
+    const r = Math.floor(Math.random() * 80) + 150;
+    const g = Math.floor(Math.random() * 80) + 150;
+    const b = Math.floor(Math.random() * 80) + 150;
+    return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+  }
+
+  private getIconeAleatorio(): string {
+    const iconesDisponiveis = [
+      'business',
+      'card',
+      'wifi',
+      'server',
+      'earth',
+      'key',
+      'lock-closed',
+      'shield-checkmark',
+      'color-palette',
+      'bar-chart',
+      'pie-chart',
+      'settings',
+      'terminal',
+      'cloud',
+    ];
+    return iconesDisponiveis[
+      Math.floor(Math.random() * iconesDisponiveis.length)
+    ];
+  }
 }
