cff01ef3a393ceb7085e8c9f5d56edcb6a3ce719
José Quintinno <josequintino@hotmail.com.br>
2025-07-02 19:27:31 -0300

    build: Gerar artefato Android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // url_api: "https://9346-2804-8aa4-290f-7000-411e-d515-e225-9533.ngrok-free.app",
-  url_api: "http://localhost:8080/defensium"
+  url_api: "https://9346-2804-8aa4-290f-7000-411e-d515-e225-9533.ngrok-free.app",
+  // url_api: "http://localhost:8080/defensium"
 };
